e51476a0ae4fa9c1753a69331eb5ab2ad8acb5e1
Alexander Nitiola <anitiola@aellacredit.com>
2025-03-24 10:32:09 +0100

    multiple pointer update

--- src/problem_solving/multitple_pointers.rs
+++ src/problem_solving/multitple_pointers.rs
@@ -6,11 +6,14 @@
 // -> Very efficient for solving problems with
 // minimal space complexity as well.
 
+// Space & Time Complexity:
+//  - Space: O(1) - uses vector with the capacity of 2 which is known at compiled-time.
+//  - Time: O(n) - single pass thruogh the while loop.
 pub fn sum_zero(list: &[i8]) -> Vec<i8> {
     let mut left = 0;
     let mut right = list.len() - 1;
 
-    let mut result: Vec<i8> = Vec::new();
+    let mut result = Vec::with_capacity(2);
 
     while left < right {
         let sum = list[left] + list[right];
@@ -20,9 +23,9 @@ pub fn sum_zero(list: &[i8]) -> Vec<i8> {
             result.push(list[right]);
             break;
         } else if sum > 0 {
-            right = right - 1
+            right = right - 1;
         } else {
-            left = left + 1
+            left = left + 1;
         }
     }
 
